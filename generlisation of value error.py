bd7fddc80dcfbc2341b24e828abd86d2959640fe
SUNDARAM MISHRA <zx.sundaram@gmail.com>
2020-01-14 01:09:16 +0530

    Delete generlisation of value error.py

--- generlisation of value error.py
+++
@@ -1,9 +0,0 @@
-def readval(valtype,requestmsg,errormsg):
-    while True:
-        val=raw_input(requestmsg+' ')
-        try:
-            val=valtype(val)
-        except ValueError:
-            print val,errormsg
-            break
-            
