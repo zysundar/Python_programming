0563b1cdd3f8247fb9581e7674e2fe974e49955c
SUNDARAM MISHRA <zx.sundaram@gmail.com>
2020-01-14 00:51:59 +0530

    Pass list as arg in class

--- Pass list as arg in class.py
+++ Pass list as arg in class.py
@@ -1,14 +1,13 @@
-class song:
+class Song:
+    
     def __init__(self,lis):
         self.lis=lis
-    def display(self):
+    
+    def Display(self):
         for i in self.lis:
-            
             print "list =",i
 
 l=(["my name is","my name is","i am from"])
-
-    
-e=song(l)
+e=Song(l)
 e.display()
-'print(e.display())'      
+#print(e.display())     
