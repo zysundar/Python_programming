a14057a1bd3be29cca2c48cc56a3f4ce2d2f8238
SUNDARAM MISHRA <zx.sundaram@gmail.com>
2020-01-14 00:13:05 +0530

    Delete create table in python.py

--- create table in python.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-c=sqlite3.connect('ete.db')
-d=c.cursor()
-d.execute(CREATE table  sundaram(rno real,name text,mb real))
-d.execute(insert into sundaram values(11505607,'sundaram',841900))
-c.commit()
-c.close()
-          
