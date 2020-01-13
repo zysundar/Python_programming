d1c1f3b35b37b47bef826d1930f88fbdcbe48163
SUNDARAM MISHRA <zx.sundaram@gmail.com>
2020-01-13 23:48:42 +0530

    Update and rename constructer.py to Constructer.py
    Constructer in class and inheritance

---
+++ Constructer.py
@@ -0,0 +1,27 @@
+class Parent:
+    parentattr=100
+    
+    def __init__(self):
+        print "Calling Parent Constructer "
+        
+    def Parentattr(self,attr):
+        Parent.attr = attr
+    
+    def Parentmethod(self):
+        print "Calling Parent Method "
+    
+    def Getattrr(self):
+        print "Get Attr= ",parent.attr
+        
+class Child(Parent):
+    
+    def __init__(self):
+        print"Calling Child Constucter"
+    
+    def Childhood(self):
+        print "Calling Childhood Method"
+c=Child()
+c.Childhood()
+c.Parentmethod()
+c.Parentattr(200)
+c.Getattr()
