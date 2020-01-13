e1f88e1cea6b741f49a45ab4caa3a1f39f69c204
SUNDARAM MISHRA <zx.sundaram@gmail.com>
2020-01-13 23:32:58 +0530

    Update and rename check range.py to Check_range.py

---
+++ Check_range.py
@@ -0,0 +1,7 @@
+def test_range(n):
+    if n in range(1,5):
+        print("Number is present in the range", str(n))
+    else:
+        print("Number is not prsent")
+print(test_range(3))
+        
--- check range.py
+++
@@ -1,7 +0,0 @@
-def test_range(n):
-    if n in range(1,5):
-        print("number is presentin the range",str(n))
-    else:
-        print("number is not prsent")
-print(test_range(3))
-        
