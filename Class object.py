1feda276b2729c556696ccdaf6bbf4b4ca95a1a1
SUNDARAM MISHRA <zx.sundaram@gmail.com>
2020-01-13 23:39:23 +0530

    Update and rename class object.py to Class object.py
    Class object example with global variable and object creation count.

--- Class object.py
+++ Class object.py
@@ -1,18 +1,21 @@
-class employee:
+class Employee:
     empcount=0
+    
     def __init__(self,name,salary):
         self.name=name
         self.salary=salary
-        employee.empcount +=1
-    def displaycount(self):
-        print "total employee=",employee.empcount
-    def displayemployee(self):
-        print "name of employee=",self.name,"salary =",self.salary
-e=employee("zara",100)
+        Employee.empcount +=1
+    
+    def Displaycount(self):
+        print "Total employee is ",employee.empcount
+    
+    def Displayemployee(self):
+        print "Name of employee is ",self.name," and salary is ",self.salary
+e=Employee("zara",100)
 e1=employee("mara",200)
 e2=employee("lara",300)
-e.displayemployee()
-e.displaycount()
+e.Displayemployee()
+e.Displaycount()
 #inbuit function for object attribute-------------
 '''hasattr(e1,"zara")
 getattr(e1,"zara")
