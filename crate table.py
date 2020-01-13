edaf8ed90cf74cceb8454942b3ca111a535bf700
SUNDARAM MISHRA <zx.sundaram@gmail.com>
2020-01-14 00:11:27 +0530

    Delete crate table.py

--- crate table.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-conn=sqlite3.connect('kika.db')
-c=conn.cursor()
-c.execute('''Create table stka(name text,address text,age real,mobileno int)''')
-c.execute("Insert Into stka Values('sanjay','lpu',29,'959241')")
-c.execute("insert into stka values('jack','lpu',28,'945673')")
-conn.commit()
-conn.close()
